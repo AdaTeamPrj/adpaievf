ccb1e936fc9ecc68ddf56d2c9e8243bc46b2f771
Fanny Boublin <fanny.boublin@gmail.com>
2022-10-13 16:41:11 +0200

    Initial version of adpaievf generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/adateam/adpaievf/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.adateam.adpaievf.security;
---
+++ src/main/java/com/adateam/adpaievf/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.adateam.adpaievf.web.rest.vm;
